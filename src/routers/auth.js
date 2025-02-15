45df5a95458063db99e864af8bcfb95a2a478ca5
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2025-02-15 11:53:13 +0200

    Changed router reset-pwd

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -40,7 +40,7 @@ router.post(
   ctrlWrapper(requestResetEmailController),
 );
 router. post (
-   '/reset-password' ,
+   '/reset-pwd' ,
    validateBody (resetPasswordSchema),
    ctrlWrapper (resetPasswordController),
 );
